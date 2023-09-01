732ecff29d08f56ec073af47b5c87e34605a7690
Salazar <praveenbaghel88990@gmail.com>
2023-09-01 22:31:42 +0530

    added firebase db to store meals

--- src/components/AvailableMeals.js
+++ src/components/AvailableMeals.js
@@ -1,37 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { WrapperCard } from "./WrapperCard";
 import classes from "./AvailableMeals.module.css";
 import { MealItem } from "./MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Chole Bhature",
-    description: "North Indian",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Vada Pav",
-    description: "Mumbai Special",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Idli Sambhar",
-    description: "South Indian Magic",
-    price: 22.99,
-  },
-  {
-    id: "m4",
-    name: "Momos",
-    description: "Darjeeling's Famous",
-    price: 12.04,
-  },
-];
-
 export const AvailableMeals = () => {
-  const availableMeals = DUMMY_MEALS.map((item) => {
+  const [meals, setMeals] = useState([]);
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://practice-database-6a346-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json"
+      );
+      const responseData = await response.json();
+      const loadedMeals = [];
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          description: responseData[key].description,
+          name: responseData[key].name,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+    };
+    fetchMeals();
+  }, []);
+  const availableMeals = meals.map((item) => {
     return (
       <MealItem
         key={item.id}
